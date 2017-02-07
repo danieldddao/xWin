dff22e7a5af7e88ab2393760616346c6d90a25fa
Daniel Dao <daniel.dddao@gmail.com>
2017-02-07 07:48:16 -0600

    added C# project and button input detection

---
+++ xWin/Library/XController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpDX.XInput;
+
+namespace xWin.Library
+{
+	class XController
+	{
+        Controller controller;
+
+        internal XController(Controller c)
+        {
+            this.controller = c;
+        }
+
+        public bool isConnected()
+        {
+            return controller.IsConnected;
+        }
+
+        public bool buttonAPressed()
+        {
+            State s = controller.GetState();
+            return s.Gamepad.Buttons.HasFlag(GamepadButtonFlags.A);
+        }
+
+        public bool buttonBPressed()
+        {
+            State s = controller.GetState();
+            return s.Gamepad.Buttons.HasFlag(GamepadButtonFlags.B);
+        }
+
+        public bool buttonXPressed()
+        {
+            State s = controller.GetState();
+            return s.Gamepad.Buttons.HasFlag(GamepadButtonFlags.X);
+        }
+
+        public bool buttonYPressed()
+        {
+            State s = controller.GetState();
+            return s.Gamepad.Buttons.HasFlag(GamepadButtonFlags.Y);
+        }
+    }
+}
---
+++ xWin/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using SharpDX.XInput;
+using xWin.Library;
+
+namespace xWin
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            XController c = new XController(new Controller(UserIndex.One));
+            bool msg = false;
+            while(!Console.KeyAvailable)
+            {
+                Thread.Sleep(300);
+                if (c.isConnected())
+                {
+                    if (!msg) {
+                        Console.WriteLine("Controller Connected!");
+                        msg = true;
+                    }
+                    if (c.buttonAPressed())
+                    {
+                        Console.WriteLine("Button A pressed!");
+                    }
+                    if (c.buttonBPressed())
+                    {
+                        Console.WriteLine("Button B pressed!");
+                    }
+                    if (c.buttonXPressed())
+                    {
+                        Console.WriteLine("Button X pressed!");
+                    }
+                    if (c.buttonYPressed())
+                    {
+                        Console.WriteLine("Button Y pressed!");
+                    }
+                } else
+                {
+                    Console.WriteLine("Controller Disconnected!");
+                    msg = false;
+                }     
+            }
+		}
+	}
+}
