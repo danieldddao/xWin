2b1629c0ca009ae9f9e7b3575bc31c09918e9f54
Daniel Dao <daniel.dddao@gmail.com>
2017-02-08 16:08:38 -0600

    added code for the remaining buttons

--- xWin/Library/XController.cs
+++ xWin/Library/XController.cs
@@ -21,28 +21,88 @@ namespace xWin.Library
             return controller.IsConnected;
         }
 
-        public bool buttonAPressed()
+        public bool isButtonAPressed()
         {
             State s = controller.GetState();
             return s.Gamepad.Buttons.HasFlag(GamepadButtonFlags.A);
         }
 
-        public bool buttonBPressed()
+        public bool isButtonBPressed()
         {
             State s = controller.GetState();
             return s.Gamepad.Buttons.HasFlag(GamepadButtonFlags.B);
         }
 
-        public bool buttonXPressed()
+        public bool isButtonXPressed()
         {
             State s = controller.GetState();
             return s.Gamepad.Buttons.HasFlag(GamepadButtonFlags.X);
         }
 
-        public bool buttonYPressed()
+        public bool isButtonYPressed()
         {
             State s = controller.GetState();
             return s.Gamepad.Buttons.HasFlag(GamepadButtonFlags.Y);
+        }
+
+        public bool isButtonBackPressed()
+        {
+            State s = controller.GetState();
+            return s.Gamepad.Buttons.HasFlag(GamepadButtonFlags.Back);
+        }
+
+        public bool isButtonDPadDownPressed()
+        {
+            State s = controller.GetState();
+            return s.Gamepad.Buttons.HasFlag(GamepadButtonFlags.DPadDown);
+        }
+
+        public bool isButtonDPadUpPressed()
+        {
+            State s = controller.GetState();
+            return s.Gamepad.Buttons.HasFlag(GamepadButtonFlags.DPadUp);
+        }
+
+        public bool isButtonDPadLeftPressed()
+        {
+            State s = controller.GetState();
+            return s.Gamepad.Buttons.HasFlag(GamepadButtonFlags.DPadLeft);
+        }
+
+        public bool isButtonDPadRightPressed()
+        {
+            State s = controller.GetState();
+            return s.Gamepad.Buttons.HasFlag(GamepadButtonFlags.DPadRight);
+        }
+
+        public bool isButtonLeftShoulderPressed()
+        {
+            State s = controller.GetState();
+            return s.Gamepad.Buttons.HasFlag(GamepadButtonFlags.LeftShoulder);
+        }
+
+        public bool isButtonRightShoulderPressed()
+        {
+            State s = controller.GetState();
+            return s.Gamepad.Buttons.HasFlag(GamepadButtonFlags.RightShoulder);
+        }
+
+        public bool isButtonLeftThumbPressed()
+        {
+            State s = controller.GetState();
+            return s.Gamepad.Buttons.HasFlag(GamepadButtonFlags.LeftThumb);
+        }
+
+        public bool isButtonRightThumbPressed()
+        {
+            State s = controller.GetState();
+            return s.Gamepad.Buttons.HasFlag(GamepadButtonFlags.RightThumb);
+        }
+
+        public bool isButtonStartPressed()
+        {
+            State s = controller.GetState();
+            return s.Gamepad.Buttons.HasFlag(GamepadButtonFlags.Start);
         }
     }
 }
--- xWin/Program.cs
+++ xWin/Program.cs
@@ -16,28 +16,68 @@ namespace xWin
             bool msg = false;
             while(!Console.KeyAvailable)
             {
-                Thread.Sleep(300);
+                Thread.Sleep(200);
                 if (c.isConnected())
                 {
                     if (!msg) {
                         Console.WriteLine("Controller Connected!");
                         msg = true;
                     }
-                    if (c.buttonAPressed())
+                    if (c.isButtonAPressed())
                     {
                         Console.WriteLine("Button A pressed!");
                     }
-                    if (c.buttonBPressed())
+                    if (c.isButtonBPressed())
                     {
                         Console.WriteLine("Button B pressed!");
                     }
-                    if (c.buttonXPressed())
+                    if (c.isButtonXPressed())
                     {
                         Console.WriteLine("Button X pressed!");
                     }
-                    if (c.buttonYPressed())
+                    if (c.isButtonYPressed())
                     {
                         Console.WriteLine("Button Y pressed!");
+                    }
+                    if (c.isButtonBackPressed())
+                    {
+                        Console.WriteLine("Button Back pressed!");
+                    }
+                    if (c.isButtonDPadDownPressed())
+                    {
+                        Console.WriteLine("Button DPadDown pressed!");
+                    }
+                    if (c.isButtonDPadUpPressed())
+                    {
+                        Console.WriteLine("Button DPadUp pressed!");
+                    }
+                    if (c.isButtonDPadLeftPressed())
+                    {
+                        Console.WriteLine("Button DPadLeft pressed!");
+                    }
+                    if (c.isButtonDPadRightPressed())
+                    {
+                        Console.WriteLine("Button DPadRight pressed!");
+                    }
+                    if (c.isButtonLeftShoulderPressed())
+                    {
+                        Console.WriteLine("Button LeftShoulder pressed!");
+                    }
+                    if (c.isButtonRightShoulderPressed())
+                    {
+                        Console.WriteLine("Button RightShoulder pressed!");
+                    }
+                    if (c.isButtonLeftThumbPressed())
+                    {
+                        Console.WriteLine("Button LeftThumb pressed!");
+                    }
+                    if (c.isButtonRightThumbPressed())
+                    {
+                        Console.WriteLine("Button RightThumb pressed!");
+                    }
+                    if (c.isButtonStartPressed())
+                    {
+                        Console.WriteLine("Button Start pressed!");
                     }
                 } else
                 {
