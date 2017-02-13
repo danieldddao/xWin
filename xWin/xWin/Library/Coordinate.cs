e05241d63ea84a97131d3e8843393fd5ea0a16ed
Matt Andress <mattandress64@gmail.com>
2017-02-13 14:53:12 -0600

    I think something is missing in the gitignore
    Signed-off-by: Matt Andress <mattandress64@gmail.com>

---
+++ xWin/xWin/Library/Coordinate.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xWin.Library
+{
+    class Coordinate
+    {
+        private short xCoord;
+        private short yCoord;
+
+        public Coordinate()
+        {
+            this.xCoord = 0;
+            this.yCoord = 0;
+        }
+
+        public Coordinate(short xCoord, short yCoord)
+        {
+            this.xCoord = xCoord;
+            this.yCoord = yCoord;
+        }
+
+        public Coordinate(Coordinate coordToCopy)
+        {
+            this.xCoord = coordToCopy.xCoord;
+            this.yCoord = coordToCopy.yCoord;
+        }
+
+        public short getXCoordinate()
+        {
+            return this.xCoord;
+        }
+
+        public short getYCoordinate()
+        {
+            return this.yCoord;
+        }
+
+        public short
+    }
+}
