17784d4071a437ce711ebd730b19b601d60b11cb
Matt Andress <mattandress64@gmail.com>
2017-02-13 18:50:34 -0600

    Really hope im not messing something up. the merge should be fun...
    Signed-off-by: Matt Andress <mattandress64@gmail.com>

--- xWin/xWin/Program.cs
+++ xWin/xWin/Program.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading;
-using SharpDX.XInput;
 using xWin.Library;
+using SharpDX.XInput;
 
 namespace xWin
 {
