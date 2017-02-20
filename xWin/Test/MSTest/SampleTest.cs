fcd4ac7609aef0e5a07589fac668fc1eb407b12b
Timothy Linhardt <Timothy Linhardt>
2017-02-19 19:59:30 -0600

    added testing

--- xWin/Test/MSTest/SampleTest.cs
+++ xWin/Test/MSTest/SampleTest.cs
@@ -1,25 +1,32 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using xWin;
 using xWin.Library;
 using Moq;
-
 namespace UnitTest
 {
-    [TestClass]
+  [TestClass]
     public class SampleTest
     {
         Mock<XController> mockController;
-
+        //Mock<StickInterpreter> mockSticks;
         [TestInitialize]
         public void Setup()
         {
             mockController = new Mock<XController>();
+            //mockSticks = new Mock<StickInterpreter>(8,0);
         }
         [TestMethod]
         public void Test()
         {
             mockController.Setup(c => c.IsConnected()).Returns(true);
             Assert.IsTrue(true);
+        }
+        [TestMethod]
+        public void TestSticks()
+        {
+            StickInterpreter sticks = new StickInterpreter(8);
+            
+            
         }
     }
 }
--- xWin/xWin/Library/StickInterpreter.cs
+++ xWin/xWin/Library/StickInterpreter.cs
@@ -6,29 +6,34 @@ using System.Threading.Tasks;
 
 namespace xWin
 {
-  class StickInterpreter
+  //null if not defined
+  public class StickInterpreter
   {
     private int angle;
     private int start;
-    public StickInterpreter(int arcs, int start)
+    public StickInterpreter(int arcs, int start=0)
     {
       this.angle = 360 / arcs;
       this.start = start;
     }
-    private int arc(int check_angle)
+    private int? arc(int? check_angle)
     {
+      if (check_angle == null)
+        return null;
       return check_angle / this.angle;
     }
-    public static int xy2deg(int x, int y) //returns angle in integer degrees
+    public static int? xy2deg(int x, int y) //returns angle in integer degrees or null if undefined
     {
+      if (x == 0 && y == 0)
+        return null;
       return Convert.ToInt32( Math.Atan2(y, x) * (180.0 / Math.PI) );
     }
 
-    public int GetRegion(int x, int y)
+    public int? GetRegion(int x, int y)
     {
       return arc(xy2deg(x, y));
     }
 
-  }
+  };
 }
 
