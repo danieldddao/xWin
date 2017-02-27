77aa86078d428d3235dbc4e58c778475cdf68aef
Matt Andress <mattandress64@gmail.com>
2017-02-27 13:02:41 -0600

    Fixed style
    Signed-off-by: Matt Andress <mattandress64@gmail.com>

--- xWin/xWin/Library/StickInterpreter.cs
+++ xWin/xWin/Library/StickInterpreter.cs
@@ -6,34 +6,39 @@ using System.Threading.Tasks;
 
 namespace xWin
 {
-  //null if not defined
-  public class StickInterpreter
-  {
-    private int angle;
-    private int start;
-    public StickInterpreter(int arcs, int start=0)
+    //null if not defined
+    public class StickInterpreter
     {
-      this.angle = 360 / arcs;
-      this.start = start;
-    }
-    private int? arc(int? check_angle)
-    {
-      if (check_angle == null)
-        return null;
-      return check_angle / this.angle;
-    }
-    public static int? xy2deg(int x, int y) //returns angle in integer degrees or null if undefined
-    {
-      if (x == 0 && y == 0)
-        return null;
-      return Convert.ToInt32( Math.Atan2(y, x) * (180.0 / Math.PI) );
-    }
+        private int angle;
+        private int start;
+        public StickInterpreter(int arcs, int start=0)
+        {
+            this.angle = 360 / arcs;
+            this.start = start;
+        }
+        private int? arc(int? check_angle)
+        {
+            if (check_angle == null)
+            {
+                return null;
+            }
 
-    public int? GetRegion(int x, int y)
-    {
-      return arc(xy2deg(x, y));
-    }
+            return check_angle / this.angle;
+        }
+        public static int? xy2deg(int x, int y) //returns angle in integer degrees or null if undefined
+        {
+            if (x == 0 && y == 0)
+            {
+                return null;
+            }
 
-  };
+            return Convert.ToInt32( Math.Atan2(y, x) * (180.0 / Math.PI) );
+        }
+
+        public int? GetRegion(int x, int y)
+        {
+            return arc(xy2deg(x, y));
+        }
+    };
 }
 
