d70e1c9aff4ade6b827390440f50bdafde89cb58
Matt Andress <mattandress64@gmail.com>
2017-03-01 11:25:30 -0600

    Started unit testing for xcontroller
    Signed-off-by: Matt Andress <mattandress64@gmail.com>

---
+++ xWin/Test/MSTest/Library/XControllerTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using xWin.Library;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using xWin.Wrapper;
+
+namespace MSTest.Library
+{
+    /// <summary>
+    /// Summary description for XControllerTest
+    /// </summary>
+    [TestClass]
+    public class XControllerTest
+    {
+        XController controller;
+        Mock<ISystemWrapper> mockSystemWrapper;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            mockSystemWrapper = new Mock<ISystemWrapper>();
+            //XController = 
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //
+            // TODO: Add test logic here
+            //
+        }
+    }
+}
