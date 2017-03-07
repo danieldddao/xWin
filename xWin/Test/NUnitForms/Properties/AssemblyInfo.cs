274c2d63287b6f497652ddf9baf54f9f1061247e
Daniel Dao <daniel-dao@uiowa.edu>
2017-03-07 15:06:46 -0600

    added project for UI Testing, probably

--- xWin/Test/NUnitForms/Properties/AssemblyInfo.cs
+++ xWin/Test/NUnitForms/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Cucumber")]
+[assembly: AssemblyTitle("NUnitForms")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Cucumber")]
+[assembly: AssemblyProduct("NUnitForms")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("d63ad74e-4cf4-4cff-908a-89b21b5b4324")]
+[assembly: Guid("6c561107-769e-4e88-96e0-ccb4b6180fc6")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
---
+++ xWin/Test/NUnitForms/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Extensions.Forms;
+using xWin.Forms;
+
+namespace NUnitForms
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        xWinPanel xWinPanel;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            xWinPanel = new xWinPanel();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
