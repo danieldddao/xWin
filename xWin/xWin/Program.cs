2184ff61cc74ef3de4e5f49870f8730b4c3d7f0d
Daniel Dao <daniel-dao@uiowa.edu>
2017-03-07 15:38:38 -0600

    fixed errors

--- xWin/xWin/Program.cs
+++ xWin/xWin/Program.cs
@@ -26,7 +26,7 @@ namespace xWin
         static void Main(string[] args)
         {
             XController c = new XController();
-            RunFormApplication();
+            //RunFormApplication();
             bool msg = false;
             /*
             while (true)
@@ -35,7 +35,7 @@ namespace xWin
                 if (c.IsConnected())
                 {
                     c.UpdateState();
-                    /*foreach(var button in c.ButtonsPressed())
+                    foreach(var button in c.ButtonsPressed())
                     {
                         Console.WriteLine(button.Key + " is pressed: " + button.Value);
                     }
@@ -48,85 +48,80 @@ namespace xWin
                         Console.WriteLine("Right " + thumb.Key + ": " + thumb.Value);
                     }
                     Console.WriteLine("Left trigger: " + c.GetLeftTrigger());
-                    Console.WriteLine("Right trigger:" + c.GetRightTrigger());*/
+                    Console.WriteLine("Right trigger:" + c.GetRightTrigger());
                     c.MoveCursor();
                     //if (c.ButtonsPressed()["A"]) c.LeftDown();
                     //else c.LeftUp();
                     //if (c.ButtonsPressed()["B"]) c.RightClick();
                     //Thread.Sleep(200);
-                    Console.Clear();
-                }
-                else
-                {
-                    Console.WriteLine("Controller Disconnected!");
-                }     
+                    Console.Clear();  
+            }*/
+
+            // Initialize DirectInput
+            var directInput = new DirectInput();
+            // Find a Joystick Guid
+            var joystickGuid = Guid.Empty;
+            foreach (var deviceInstance in directInput.GetDevices(SharpDX.DirectInput.DeviceType.Gamepad, DeviceEnumerationFlags.AllDevices))
+                joystickGuid = deviceInstance.InstanceGuid;
+            // If Gamepad not found, look for a Joystick
+            if (joystickGuid == Guid.Empty)
+                foreach (var deviceInstance in directInput.GetDevices(SharpDX.DirectInput.DeviceType.Joystick, DeviceEnumerationFlags.AllDevices))
+                    joystickGuid = deviceInstance.InstanceGuid;
+            // If Joystick not found, throws an error
+            if (joystickGuid == Guid.Empty)
+            {
+                Console.WriteLine("No joystick/Gamepad found.");
+                Console.ReadKey();
+                Environment.Exit(1);
             }
-            */
-        
-           // Initialize DirectInput
-           var directInput = new DirectInput();
-           // Find a Joystick Guid
-           var joystickGuid = Guid.Empty;
-           foreach (var deviceInstance in directInput.GetDevices(SharpDX.DirectInput.DeviceType.Gamepad, DeviceEnumerationFlags.AllDevices))
-               joystickGuid = deviceInstance.InstanceGuid;
-           // If Gamepad not found, look for a Joystick
-           if (joystickGuid == Guid.Empty)
-               foreach (var deviceInstance in directInput.GetDevices(SharpDX.DirectInput.DeviceType.Joystick, DeviceEnumerationFlags.AllDevices))
-                   joystickGuid = deviceInstance.InstanceGuid;
-           // If Joystick not found, throws an error
-           if (joystickGuid == Guid.Empty)
-           {
-               Console.WriteLine("No joystick/Gamepad found.");
-               Console.ReadKey();
-               Environment.Exit(1);
-           }
-           // Instantiate the joystick
-           var joystick = new Joystick(directInput, joystickGuid);
-           Console.WriteLine("Found Joystick/Gamepad with GUID: {0}", joystickGuid);
-           // Set BufferSize in order to use buffered data.
-           joystick.Properties.BufferSize = 128;
-           foreach (DeviceObjectInstance doi in joystick.GetObjects(DeviceObjectTypeFlags.Axis))
-           {
-               joystick.GetObjectPropertiesById(doi.ObjectId).Range = new InputRange(-32767, 32767);
-           }
-           // Acquire the joystick
-           joystick.Acquire();
-           // Poll events from joystick
-           while (true)
-           {
-               joystick.Poll();
-               var datas = joystick.GetCurrentState();
-               var state = DI2XI.di2xi(datas);
-               foreach (GamepadButtonFlags b in Enum.GetValues(typeof(GamepadButtonFlags)))
-               {
-                   if (state.Gamepad.Buttons.HasFlag(b))
-                   {
-                       Console.Write(b);
-                       Console.Write(",");
-                   }
-               }
-               if (state.Gamepad.LeftTrigger > 0)
-                   Console.Write("LeftTrigger,");
-               if (state.Gamepad.RightTrigger > 0)
-                   Console.Write("RightTrigger,");
-               
-               var ps = new PolarStick(state.Gamepad.LeftThumbX, state.Gamepad.LeftThumbY, 1000);
-               Console.WriteLine("");
-               Console.WriteLine("Left Stick:");
-               Console.Write("Direction: ");
-               Console.WriteLine(ps.theta);
-               Console.Write("Distance: ");
-               Console.WriteLine(ps.R);
-               ps = new PolarStick(state.Gamepad.RightThumbX, state.Gamepad.RightThumbY, 1000);
-               Console.WriteLine("");
-               Console.WriteLine("Right Stick:");
-               Console.Write("Direction: ");
-               Console.WriteLine(ps.theta);
-               Console.Write("Distance: ");
-               Console.WriteLine(ps.R);
-               Thread.Sleep(300);
-               Console.Clear();
-           }
-        
+            // Instantiate the joystick
+            var joystick = new Joystick(directInput, joystickGuid);
+            Console.WriteLine("Found Joystick/Gamepad with GUID: {0}", joystickGuid);
+            // Set BufferSize in order to use buffered data.
+            joystick.Properties.BufferSize = 128;
+            foreach (DeviceObjectInstance doi in joystick.GetObjects(DeviceObjectTypeFlags.Axis))
+            {
+                joystick.GetObjectPropertiesById(doi.ObjectId).Range = new InputRange(-32767, 32767);
+            }
+            // Acquire the joystick
+            joystick.Acquire();
+            // Poll events from joystick
+            while (true)
+            {
+                joystick.Poll();
+                var datas = joystick.GetCurrentState();
+                var state = DI2XI.di2xi(datas);
+                foreach (GamepadButtonFlags b in Enum.GetValues(typeof(GamepadButtonFlags)))
+                {
+                    if (state.Gamepad.Buttons.HasFlag(b))
+                    {
+                        Console.Write(b);
+                        Console.Write(",");
+                    }
+                }
+                if (state.Gamepad.LeftTrigger > 0)
+                    Console.Write("LeftTrigger,");
+                if (state.Gamepad.RightTrigger > 0)
+                    Console.Write("RightTrigger,");
+
+                var ps = new PolarStick(state.Gamepad.LeftThumbX, state.Gamepad.LeftThumbY, 1000);
+                Console.WriteLine("");
+                Console.WriteLine("Left Stick:");
+                Console.Write("Direction: ");
+                Console.WriteLine(ps.theta);
+                Console.Write("Distance: ");
+                Console.WriteLine(ps.R);
+                ps = new PolarStick(state.Gamepad.RightThumbX, state.Gamepad.RightThumbY, 1000);
+                Console.WriteLine("");
+                Console.WriteLine("Right Stick:");
+                Console.Write("Direction: ");
+                Console.WriteLine(ps.theta);
+                Console.Write("Distance: ");
+                Console.WriteLine(ps.R);
+                Thread.Sleep(300);
+                Console.Clear();
+            }
+
+        }
     }
 }
